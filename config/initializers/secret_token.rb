276508893fd923e3432a738a5728d1b296ecf35e
App Academy Student <student@appacademy.io>
2014-02-04 08:21:02 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookClub::Application.config.secret_token = 'f65639da397caa53ef130f2a7d2c8aa31f8dd1d272fdfa1cf162985918f51f4dfefe89f2de05baafaf8b7aa8d7e04ba89dffed3c7be725b40f5f034dd9fad1b8'
